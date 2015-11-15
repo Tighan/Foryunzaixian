67e34e0edf85928c1f804914295135da9d186594
Tiggg <421346755@qq.com>
2015-11-15 11:54:15 +0800

    refresh

--- seleniumforalipay.py
+++ seleniumforalipay.py
@@ -7,21 +7,23 @@ import time
 
 display = Display(visible=0, size=(800, 600))
 display.start()
-
+count=0
 w=webdriver.Firefox()
 w.get('https://cschannel.alipay.com/portal.htm?sourceId=114&enterurl=https://cshall.alipay.com/hall/index.htm')
-w.find_element_by_css_selector("textarea[seed=\"inputField-content\"]").send_keys(u"退款未到帐")
-time.sleep(2)
-w.find_element_by_id("main-submit-btn").click()
-w.find_element_by_css_selector("textarea[seed=\"inputField-content\"]").send_keys(u"退款未到帐")
-time.sleep(2)
-w.find_element_by_id("main-submit-btn").click()
-time.sleep(10)
-w.find_element_by_css_selector("a[seed=\"PCportal_self_toYunzaixian\"]").click()
-time.sleep(2)
-num=w.find_elements_by_css_selector("div[class=\"content\"]")[4].text
-print num
-w.quit()
+while count <=20:
+	w.find_element_by_css_selector("textarea[seed=\"inputField-content\"]").send_keys(u"退款未到帐")
+	time.sleep(2)
+	w.find_element_by_id("main-submit-btn").click()
+	w.find_element_by_css_selector("textarea[seed=\"inputField-content\"]").send_keys(u"退款未到帐")
+	time.sleep(2)
+	w.find_element_by_id("main-submit-btn").click()
+	print "add custom times:",count
+	time.sleep(10)
+	w.find_element_by_css_selector("a[seed=\"PCportal_self_toYunzaixian\"]").click()
+	time.sleep(2)
+	w.refresh()
+	count+=1
+	
 
 
 display.stop()
