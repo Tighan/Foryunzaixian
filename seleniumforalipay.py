916f7096607ecd545da6e3420bae1f0f8940789e
root <root@VM-30-129-ubuntu.(none)>
2015-11-15 13:14:32 +0800

    aaa

--- seleniumforalipay.py
+++ seleniumforalipay.py
@@ -8,9 +8,11 @@ import time
 display = Display(visible=0, size=(800, 600))
 display.start()
 count=0
-w=webdriver.Firefox()
-w.get('https://cschannel.alipay.com/portal.htm?sourceId=114&enterurl=https://cshall.alipay.com/hall/index.htm')
 while count <=20:
+
+        w=webdriver.Firefox()
+
+        w.get('https://cschannel.alipay.com/portal.htm?sourceId=114&enterurl=https://cshall.alipay.com/hall/index.htm')
 	w.find_element_by_css_selector("textarea[seed=\"inputField-content\"]").send_keys(u"退款未到帐")
 	time.sleep(2)
 	w.find_element_by_id("main-submit-btn").click()
@@ -21,9 +23,9 @@ while count <=20:
 	time.sleep(10)
 	w.find_element_by_css_selector("a[seed=\"PCportal_self_toYunzaixian\"]").click()
 	time.sleep(2)
-	w.refresh()
+	w.quit()
 	count+=1
-	
+	time.sleep(30)
 
 
 display.stop()
